c017cb2c8ab3ba2fa18f38bda3f530b6fa2e1d29
sara dabrowska <sara.dabrowska.197@gmail.com>
2018-06-23 11:05:04 +0200

    wip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.action_mailer.default_url_options = { host: 'www.leszczyna.wzks.uj.edu.pl/15_daborwska/planner' }
+  config.action_mailer.default_url_options = { host: 'www.leszczyna.wzks.uj.edu.pl/15_dabrowska/planner' }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
